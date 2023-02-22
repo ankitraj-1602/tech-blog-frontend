d4c585dcfc33d4e53e25311b1b60c8cc34078ada
Ankit Raj <nycankit@gmail.com>
2023-02-22 12:24:42 +0530

    frontend of blog application

---
+++ src/components/AddBlog.js
@@ -0,0 +1,118 @@
+import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useStyles } from "./utils";
+
+const labelStyles = { mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" };
+const AddBlog = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    title: "",
+    description: "",
+    imageURL: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async () => {
+    const res = await axios
+      .post("http://localhost:5000/api/blog/add", {
+        title: inputs.title,
+        description: inputs.description,
+        image: inputs.imageURL,
+        user: localStorage.getItem("userId"),
+      })
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    sendRequest()
+      .then((data) => console.log(data))
+      .then(() => navigate("/blogs"));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          border={3}
+          borderColor="black"
+          borderRadius={10}
+          boxShadow="10px 10px 20px grey"
+          padding={3}
+          margin={"auto"}
+          marginTop={3}
+          display="flex"
+          flexDirection={"column"}
+          width={"60%"}
+          alignItems="center"
+        >
+          <Typography
+            className={classes.font}
+           
+            padding={3}
+            color="grey"
+            variant="h3"
+            textAlign={"center"}
+          >
+           Add Blog
+          </Typography>
+          <InputLabel className={classes.font} sx={labelStyles}>
+            Title
+          </InputLabel>
+          <TextField
+            className={classes.font}
+            name="title"
+            onChange={handleChange}
+            value={inputs.title}
+            margin="auto"
+            variant="outlined"
+            sx={{width:700}}
+          />
+          <InputLabel className={classes.font} sx={labelStyles}>
+            Description
+          </InputLabel>
+          <TextField
+            className={classes.font}
+            name="description"
+            onChange={handleChange}
+            value={inputs.description}
+            margin="auto"
+            variant="outlined"
+            sx={{width:700}}
+          />
+          <InputLabel className={classes.font} sx={labelStyles}>
+            ImageURL
+          </InputLabel>
+          <TextField
+            className={classes.font}
+            name="imageURL"
+            onChange={handleChange}
+            value={inputs.imageURL}
+            margin="auto"
+            variant="outlined"
+            sx={{width:700}}
+          />
+          <Button
+            sx={{ width:200, mt: 3, borderRadius: 5 }}
+            variant="contained"
+            color="success"
+            type="submit"
+            
+          >
+            Submit
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default AddBlog;
